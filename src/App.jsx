7af60d47c6c362342e353ecc55a58202db583f8c
Jeff Soriano <soriano.jeffm@gmail.com>
2023-11-06 16:28:48 -0800

    Implement Navbar component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Navbar } from './components'
+
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      {/* <Welcome /> */}
+    </div>
+    {/* <Services />
+    <Transactions />
+    <Footer /> */}
+  </div>
+)
+
+export default App
